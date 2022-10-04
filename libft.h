904c20396aa00b33b6b62f5b5efa0e4d9b8eb4c8
Keisuke Goshima <kgoshima@c1r28s7.42tokyo.jp>
2022-10-05 07:08:30 +0900

    ft_is

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgoshima <kgoshima@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 06:57:34 by kgoshima          #+#    #+#             */
+/*   Updated: 2022/10/05 06:59:12 by kgoshima         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define	LIBFT_H
+# include <stddef.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+
+#endif
